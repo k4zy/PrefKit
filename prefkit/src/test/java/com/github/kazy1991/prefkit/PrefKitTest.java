b7711a01bed85e95970059d19d3a04d5ff2f31e2
kazuki-yoshida <kzk.yshd@gmail.com>
2017-04-01 22:03:03 +0900

    Fix Test

--- prefkit/src/test/java/com/github/kazy1991/prefkit/PrefKitTest.java
+++ prefkit/src/test/java/com/github/kazy1991/prefkit/PrefKitTest.java
@@ -7,16 +7,19 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowApplication;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 
 @Config(manifest = Config.NONE)
 @RunWith(RobolectricTestRunner.class)
 public class PrefKitTest {
 
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void callTest() throws Exception {
         PrefKit prefKit = new PrefKit(ShadowApplication.getInstance().getApplicationContext());
         TestSchema schema = prefKit.create(TestSchema.class);
         schema.getSampleValue(false);
         schema.setSampleValue(true);
+        assertThat(true).isTrue();
     }
 }
